752eb5844f7b0072d6ca4c0113a6e9fe423be781
LanceStasinski <lance.stasinski@gmail.com>
2021-07-15 12:31:12 -0400

    refactor: Refactor confirm ending function to one line of code

--- algorithms/endsWith.js
+++ algorithms/endsWith.js
@@ -1,11 +1,4 @@
 function confirmEnding(str, target) {
-  const START = str.length - target.length;
-  const END = str.length;
-  const SUBSTRING = str.substring(START, END);
-
-  return (SUBSTRING === target);
+  return str.slice(str.length - target.length) === target;
 }
 
-confirmEnding('lance', 'ance')
-
-//try another method, maybe splice, that allows you choose start and end chars
