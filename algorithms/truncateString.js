72789a95a2fc5b46a6fda42630b6c782114d3af7
LanceStasinski <lance.stasinski@gmail.com>
2021-07-15 13:32:30 -0400

    refactor: Refactore truncateString to use a ternary operator

--- algorithms/truncateString.js
+++ algorithms/truncateString.js
@@ -1,6 +1,3 @@
 function truncateString(str, num) {
-  if (str.length > num) {
-    str = str.slice(0, num) + '...';
-  }
-  return str;
+  return str.length > num ? str.slice(0, num) + '...' : str;
 }
