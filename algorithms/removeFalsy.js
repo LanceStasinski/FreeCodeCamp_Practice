8077c6260bde0336f9a65d405f6a298089bcd783
LanceStasinski <lance.stasinski@gmail.com>
2021-07-15 14:59:53 -0400

    refactor: Refactor removeFalsey using an arrow function and Array.prototype.filter()

--- algorithms/removeFalsy.js
+++ algorithms/removeFalsy.js
@@ -1,9 +1,1 @@
-function bouncer(arr) {
-  let newArr = []
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i]) {
-      newArr.push(arr[i])
-    }
-  }
-  return newArr;
-}
+const bouncer = (arr) => arr.filter(Boolean)
