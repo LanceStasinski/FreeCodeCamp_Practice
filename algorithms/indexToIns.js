b362d9714af4052c1b0107d592f7ae23cc89c950
LanceStasinski <lance.stasinski@gmail.com>
2021-07-15 15:28:49 -0400

    refactor: Refactor getIndexToIns to an arrow function that uses Array.prototype.filter()

--- algorithms/indexToIns.js
+++ algorithms/indexToIns.js
@@ -1,9 +1,3 @@
-function getIndexToIns(arr, num) {
-  arr.sort((a,b) => a-b)
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] >= num) {
-      return i;
-    }
-  }
-  return arr.length
-}
+const getIndexToIns = (arr, num) => arr.filter(val => num > val).length;
+
+//return length of array for which input number is greater than previous elements
