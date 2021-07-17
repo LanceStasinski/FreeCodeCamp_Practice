e3baba0589928ac446b797f2a3fedf0ac380250a
LanceStasinski <lance.stasinski@gmail.com>
2021-07-17 08:43:42 -0400

    refactor: Refactor destroyer to use a spread operator in arguments

--- algorithms/removeElements.js
+++ algorithms/removeElements.js
@@ -1,7 +1,1 @@
-function destroyer(arr) {
-  let argArr = []
-  for (let i = 1; i < arguments.length; i++) {
-    argArr.push(arguments[i])
-  }
-  return arr.filter(val => !argArr.includes(val))
-}
+const destroyer = (arr, ...valsToRemove) =>  arr.filter(val => !valsToRemove.includes(val))
