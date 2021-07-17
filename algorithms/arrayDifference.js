86e16b9c10104381d58251af354ee885452d2b9e
LanceStasinski <lance.stasinski@gmail.com>
2021-07-17 08:17:40 -0400

    refactor: Refactor diffArray function to an arrow function

--- algorithms/arrayDifference.js
+++ algorithms/arrayDifference.js
@@ -1,3 +1,3 @@
-function diffArray(arr1, arr2) {
-  return arr1.concat(arr2).filter(val => !arr1.includes(val) || !arr2.includes(val))
-}
+const diffArray = (arr1, arr2) => arr1
+  .concat(arr2)
+  .filter(val => !arr1.includes(val) || !arr2.includes(val))
