76ea9360913102c9bd74e1ef5b2f7d71c499453d
LanceStasinski <lance.stasinski@gmail.com>
2021-07-19 11:51:49 -0400

    refactor: Refactor pairElement using a pair object and map()  instead of a ternary operator and a for loop

--- algorithms/dnaParing.js
+++ algorithms/dnaParing.js
@@ -1,16 +1,11 @@
 function pairElement(str) {
   const ACID_ARR = str.split('')
-  let fullArray = [];
-
-  for (let i = 0; i < str.length; i++) {
-    let pairArray = [];
-    pairArray.push(ACID_ARR[i])
-    const MATCH = ACID_ARR[i] === 'A' ? 'T'
-    : ACID_ARR[i] === 'T' ? 'A'
-    : ACID_ARR[i] === 'G' ? 'C'
-    : 'G';
-    pairArray.push(MATCH)
-    fullArray.push(pairArray)
+  const PAIRS = {
+    A: 'T',
+    T: 'A',
+    C: 'G',
+    G: 'C'
   }
-  return fullArray;
+
+  return ACID_ARR.map(val => [val, PAIRS[val]])
 }
