2499c84e1b6ef1843650ce756b932b5b98f8752b
LanceStasinski <lance.stasinski@gmail.com>
2021-07-19 19:33:29 -0400

    refactor: Refactor binaryAgent using arrow functions, a spread operator, and map()

--- algorithms/translateBinary.js
+++ algorithms/translateBinary.js
@@ -1,10 +1,1 @@
-function binaryAgent(str) {
-  const arr = str.split(' ')
-  const stringArr = [];
-
-  for(let i = 0; i < arr.length; i++) {
-    stringArr.push(String.fromCharCode(parseInt(arr[i], 2)))
-  }
-
-  return stringArr.join('');
-}
+const binaryAgent = (str) => String.fromCharCode(...str.split(' ').map(val => parseInt(val, 2)))
