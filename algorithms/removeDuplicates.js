1f9f43e5e0485bba3984b504b3f7c33f4993ea28
LanceStasinski <lance.stasinski@gmail.com>
2021-07-19 13:29:12 -0400

    refactor: Refactor uniteUnique by concatonating argument arrays and use Set() to remove duplicate values

--- algorithms/removeDuplicates.js
+++ algorithms/removeDuplicates.js
@@ -1,15 +1,1 @@
-function uniteUnique(arr) {
-  let finalArr = [];
-
-  for (let i = 0; i < arguments.length; i++) {
-    let argArr = arguments[i];
-
-    for (let j = 0; j < argArr.length; i++) {
-      let value = argArr[j]
-      if (arr.indexOf(value) < 0) {
-        finalArr.push(value)
-      }
-    }
-  }
-  return finalArr;
-}
+const uniteUnique = (...arr) => [...new Set(arr.flat())]
