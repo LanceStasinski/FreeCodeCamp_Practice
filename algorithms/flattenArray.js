c29de6ca8a38c8d11d56a3c3d158b3ec33b1b61a
LanceStasinski <lance.stasinski@gmail.com>
2021-07-19 19:10:09 -0400

    feat: Flatten array of any dimension without using .flat()

---
+++ algorithms/flattenArray.js
@@ -0,0 +1,12 @@
+function steamrollArray(arr) {
+ let flat = [];
+
+ for(let i = 0; i < arr.length; i++) {
+   if (Array.isArray(arr[i])) {
+     flat.concat(...steamrollArray(arr[i]))
+   } else {
+     flat.push(arr[i])
+   }
+ }
+ return flat;
+}
