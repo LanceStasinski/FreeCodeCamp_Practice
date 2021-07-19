ad4b84034b77425ec2f197b449f385cd68b77826
LanceStasinski <lance.stasinski@gmail.com>
2021-07-19 13:58:16 -0400

    refactor: Refactor using object lookup and regex

--- algorithms/convertSpecialToHTML.js
+++ algorithms/convertSpecialToHTML.js
@@ -1,15 +1,11 @@
 function convertHTML(str) {
-  const NEW_ARR = []
-  for (let i = 0; i < str.length; i++) {
-    let char = str.charAt(i)
-    let symbol = char === '>' ? '&gt;'
-    : char === '<' ? '&lt;'
-    : char === '&' ? '&amp;'
-    : char === '"' ? '&quot;'
-    : char === '\'' ? '&apos;'
-    : char;
 
-    NEW_ARR.push(symbol)
-  }
-  return NEW_ARR.join('')
+  const SYMBOLS = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&apos;"
+  };
+  return str.replace(/([&<>\"'])/g, match => SYMBOLS[match]);
 }
