6c10f8e80a76a0f00396d06c8a477dd42218bd57
LanceStasinski <lance.stasinski@gmail.com>
2021-07-19 11:30:40 -0400

    refactor: Refactor myReplace using a ternary operator and regex

--- algorithms/replaceWord.js
+++ algorithms/replaceWord.js
@@ -1,10 +1,7 @@
 function myReplace(str, before, after) {
-  const LOCATION = str.search(before);
-  if (str.charAt(LOCATION) === str.charAt(LOCATION).toUpperCase()) {
-    after = after.charAt(0).toUpperCase() + after.slice(1);
-  } else if (str.charAt(LOCATION) === str.charAt(LOCATION).toLowerCase()) {
-    after = after.charAt(0).toLowerCase() + after.slice(1);
-  }
+  const START_CHAR = str.charAt(str.search(before))
+  after = /[A-Z]/.test(START_CHAR) ? after.charAt(0).toUpperCase() + after.slice(1)
+  : after = after.charAt(0).toLowerCase() + after.slice(1);
 
-  return str.replace(before, after);
+  return str.replace(before, after)
 }
