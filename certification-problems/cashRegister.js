cf8850ef605ce1c4bd3e3a68fb9f68b1e5d3c747
LanceStasinski <lance.stasinski@gmail.com>
2021-07-21 16:48:10 -0400

    refactor: Refactor while loop by removing if statement and by programmatically creating changeArr

--- certification-problems/cashRegister.js
+++ certification-problems/cashRegister.js
@@ -2,7 +2,7 @@ function checkCashRegister(price, cash, cid) {
   let changeDue = (cash - price) * 100;
   const values = [0.01, 0.05, 0.10, 0.25, 1, 5, 10, 20, 100].map(val => val * 100).reverse();
   let returnObj = {status: 'INSUFFICIENT_FUNDS', change: []}
-  let changeArr = [["PENNY", 0], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]].reverse()
+  let changeArr = cid.map(val => val = [val[0], 0]).reverse()
   let revCid = cid.reverse().map(val => parseFloat((val[1] * 100).toFixed(2)))
   let sumInRegister = revCid.reduce((a, b) => a + b)
 
@@ -13,12 +13,10 @@ function checkCashRegister(price, cash, cid) {
   }
 
   for(let i = 0; i < revCid.length; i++) {
-    while(revCid[i] > 0) {
-      if(changeDue - values[i] >= 0) {
-        changeDue -= values[i];
-        changeArr[i][1] += values[i];
-        revCid[i] -= values[i]
-      } else break;
+    while(revCid[i] > 0 && changeDue - values[i] >= 0) {
+      changeDue -= values[i];
+      changeArr[i][1] += values[i];
+      revCid[i] -= values[i]
     }
     if (changeDue === 0) break;
   }
